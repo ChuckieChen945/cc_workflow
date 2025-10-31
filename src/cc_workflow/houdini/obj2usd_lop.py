43afc0ee1178b9c429bdac30f151ed138f127d59
Chuckie Chen <chuckie_chen@outlook.com>
2025-10-31 15:17:04 +0800

    Refactor MaterialX basic material template and improve obj2usd_lop.py for better node management

--- src/cc_workflow/houdini/obj2usd_lop.py
+++ src/cc_workflow/houdini/obj2usd_lop.py
@@ -6,12 +6,9 @@ import hou
 hip_dir = Path(hou.hipFile.path()).parent.parent
 root_folder = (hip_dir / "obj").resolve()
 
-
-# 清空已有子节点
+# 已有子节点
 parent = hou.pwd().parent()
-for child in parent.children():
-    if child.name().startswith("import_"):
-        child.destroy()
+existing_nodes = [child for child in parent.children() if child.name().startswith("import_")]
 
 # 遍历 obj 文件
 obj_folders = [f for f in root_folder.iterdir() if f.is_dir()]
@@ -21,16 +18,17 @@ for folder in obj_folders:
 
 for path in obj_files:
     # 在 Houdini 中用相对路径表示层级
-    rel_path = path.relative_to(root_folder)
-    # 替换路径分隔符为下划线创建节点名
-    name = "import_" + "_".join(rel_path.with_suffix("").parts)
+    name = "import_" + "_".join(path.name.split(".")[:-1])
+    if name in existing_nodes:
+        continue
 
     node = parent.createNode("create_assets", name)
     node.parm("file_path").set(str(path))
+    node.cook(force=True)  # 或强制计算节点
 
     out_null = parent.createNode(
         "null",
-        "out_" + "_".join(rel_path.with_suffix("").parts),
+        "out_" + "_".join(path.name.split(".")[:-1]),
     )
     out_null.setInput(0, node)
     out_null.moveToGoodPosition()
